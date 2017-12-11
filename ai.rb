e8b41d9ac2441e5cebf84c19c008662e7c1d5b70
jeff1st <jeff1st@gmail.com>
2017-12-11 23:34:42 +0100

    a little bit of decoration

--- ai.rb
+++ ai.rb
@@ -15,65 +15,50 @@ end
 class GameIA
 include GameHelpers 
 
-  attr_accessor :secret, :results
+  attr_reader :secret, :results, :attempts
+  attr_accessor :guess
 
   def initialize
     @secret_count = Hash.new(0)
     @secret = set_secret
-    @attempts = 0
+    @attempts = 10
+    @guess = []
     init_game
     fill_secret_count
-#    ingame
   end
 
-  def init_game
-    @guess = []
-    @guess_count = Hash.new(0)
-    @results = ["i", "j", "k", "l"]
+  def reduce
+    @attempts -= 1
   end
 
-  def ingame
-    while @attempts <= 12
-      4.times do
-        puts "hello, make a guess"
-        COLORS.each_with_index do |color, index|
-          puts "#{index} - #{color}"
-        end
-        answer = gets.chomp
-        @guess << COLORS[answer.to_i]
-      end
-      @attempts += 1
-      check_guess
-      puts @guess.inspect
-      puts @results.inspect
-      say_win if @results.all? { |res| res == "O" }
-      init_game      
-    end
-    say_loose
+  def check_guess
     init_game
-  end
+    @guess.each_with_index do |color, index|
+      if is_correct?(color, index, @secret)
+        @results[index] << "O"
+        @guess_count[color] += 1
+      end
+    end
 
-  private
-    def check_guess
-      @guess.each_with_index do |color, index|
-        if is_correct?(color, index, @secret)
-          @results[index] << "O"
+    @guess.each_with_index do |color, index|
+      if is_misplaced?(color, index, @secret)
+        if @guess_count[color] < @secret_count[color]
+          @results[index] << "M"
           @guess_count[color] += 1
         end
       end
+    end
 
-      @guess.each_with_index do |color, index|
-        if is_misplaced?(color, index, @secret)
-          if @guess_count[color] < @secret_count[color]
-            @results[index] << "M"
-            @guess_count[color] += 1
-          end
-        end
-      end
+    (0..4).each do |i|
+      @results[i] = "X" if @results[i] == ""
+    end
+  end
 
-      (0..4).each do |i|
-        @results[i] = "X" if @results[i] == ""
-      end
+  private
+
+    def init_game
+      @guess_count = Hash.new(0)
+      @results = ["", "", "", ""]
     end
 
     def set_secret
@@ -86,16 +71,6 @@ include GameHelpers
       @secret.each do |color|
         @secret_count[color] += 1
       end
-    end
-
-    def say_win
-      puts "you won!!"
-      initialize
-    end
-
-    def say_loose
-      puts "you loose!"
-      initialize
     end
 end
 
--- app.rb
+++ app.rb
@@ -2,24 +2,67 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './ai'
 
+def check_message(remains, result)
+  message = ""
+
+  if result == ["O", "O", "O", "O"]
+    message = "Wonderful!, you found it!"
+
+  elsif result != ["O", "O", "O", "O"] && remains == 0
+    message = "Wow, too bad, not found!"
+
+  else
+    message = "Only #{remains} attempt(s) left!"
+  end
+
+  return message
+end
+
+def keep_last_selection(res)
+  transform = []
+  colors = ["yellow", "blue", "red", "green", "pink", "orange"]
+  (0...4).each { |i| transform[i] = colors.index(res[i])+1 }
+  return transform
+end
+
 enable :sessions
 game = GameIA.new
-results = []
 
 get '/welcome' do
-  erb :mastermind, { layout: :layout }
+  erb :mastermind, { :layout => :layout }
 end
 
 get '/game' do
-  session[:secret] = game.secret
-  res = game.results
-  puts res.inspect
-  session[:secret] << res
-  results << session[:secret]
-  erb :game, { layout: :layout,
+  session[:results] ||= []
+  sel ||= []
+
+  if params[:field1]
+    game.guess = [params[:field1], params[:field2], params[:field3], params[:field4]]
+    game.check_guess
+
+    session[:results] << (game.guess << game.results)
+
+    game.reduce
+
+    message = check_message(game.attempts, game.results)
+    remains = game.attempts
+    res = game.results
+    sel = keep_last_selection(session[:results][-1])
+  end
+
+  erb :game, { :layout => :layout,
                  :locals => {
-                    :results => results
+                    :message => message,
+                    :results => session[:results],
+                    :res => res,
+                    :remains => remains,
+                    :sel => sel
                  }
                }
 end
 
+get '/reset' do
+  session[:results] = []
+  game = GameIA.new
+  redirect '/game'
+end
